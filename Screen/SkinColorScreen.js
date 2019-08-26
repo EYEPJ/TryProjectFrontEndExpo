2f4b07a78cd29a21a1203f4e23bfe1537ed5585e
iceasdza <ice_ro.patis@hotmail.com>
2019-08-26 21:37:32 +0700

    [edok]

--- Screen/SkinColorScreen.js
+++ Screen/SkinColorScreen.js
@@ -1,79 +1,104 @@
 //import liraries
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import axios from 'axios';
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground
+} from "react-native";
+import axios from "axios";
 
 // create a component
 class SkinColorScreen extends Component {
+  static navigationOptions = {
+    title: "Select Your Skin Color"
+  };
+  state = {
+    data: [],
+    user: this.props.navigation.state.params.user
+  };
 
-    state = {
-        data: [],
-        user: this.props.navigation.state.params.user
-    }
+  setUserinfo = skinId => {
+    console.log(skinId);
+    this.state.user.skin = skinId;
+    this.createUser();
+    this.props.navigation.navigate("Profile", {
+      user: this.state.user
+    });
+    console.log(this.state.user);
+  };
 
-
-        
-    setUserinfo = (skinId) => {
-        console.log(skinId)
-        this.state.user.skin = skinId;
-        this.createUser();
-        this.props.navigation.navigate('Profile', {
-          user: this.state.user
-        });
-        console.log(this.state.user)
-    }
-
-    createUser = () => {
-        axios.post('http://3.92.192.76:8000/createUser/', {
-            fbId: this.state.user.fbId,
-            userName: this.state.user.name,
-            userProfile: this.state.user.profilePic,
-            userBodyPictureUrl: "",
-            userGender: this.state.user.gender,
-            shapeId: this.state.user.shape,
-            skinColorId: this.state.user.skin
-        }).then(res => {
-            console.log(res);
-            console.log(res.data);
+  createUser = () => {
+    axios
+      .post("http://3.92.192.76:8000/createUser/", {
+        fbId: this.state.user.fbId,
+        userName: this.state.user.name,
+        userProfile: this.state.user.profilePic,
+        userBodyPictureUrl: "",
+        userGender: this.state.user.gender,
+        shapeId: this.state.user.shape,
+        skinColorId: this.state.user.skin
       })
-    }
-        
-    componentDidMount() {
-        axios.get('http://3.92.192.76:8000/skinColorManagement/')
-            .then(res => {
-        const data = res.data;
-        console.log(data)
-        this.setState({ data });
-        console.log('test', data)
-      })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      });
+  };
+
+  componentDidMount() {
+    axios.get("http://3.92.192.76:8000/skinColorManagement/").then(res => {
+      const data = res.data;
+      console.log(data);
+      this.setState({ data });
+      console.log("test", data);
+    });
   }
 
-
-    render() {
-        return (
-            <View style={styles.container}>
-                {
-                    this.state.data.map((v,index) => {
-                        return (
-                            <TouchableOpacity onPress={() => this.setUserinfo(v.id)}> 
-                                <View style={{width: 50, height: 50, backgroundColor:v.skinColorCode}}></View>  
-                            </TouchableOpacity>
-                        )
-          })
-        }
-            </View>
-        );
-    }
+  render() {
+    return (
+      <ImageBackground
+        source={require("../Image/bg.png")}
+        style={styles.ImageBackgroundStyle}
+      >
+        <View style={styles.container}>
+            {this.state.data.map((v, index) => {
+              return (
+                <TouchableOpacity onPress={() => this.setUserinfo(v.id)}>
+                  <View
+                    style={{
+                      width: 60,
+                      height: 60,
+                      borderRadius: 50,
+                      margin: 20,
+                      backgroundColor: v.skinColorCode
+                    }}
+                  />
+                </TouchableOpacity>
+              );
+            })}
+        </View>
+      </ImageBackground>
+    );
+  }
 }
 
 // define your styles
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
-    },
+  ImageBackgroundStyle: {
+    width: "100%",
+    marginTop: 0,
+    height: "200%"
+  },
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 50,
+
+    
+    
+  },
+
 });
 
 //make this component available to the app
